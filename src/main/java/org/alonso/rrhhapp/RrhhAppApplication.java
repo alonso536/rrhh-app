073d2429fe966ce748026a879237672ff5dcf85c
alonso536 <josealonsodiazleiva@gmail.com>
2024-01-03 10:38:37 -0300

    First commit

---
+++ src/main/java/org/alonso/rrhhapp/RrhhAppApplication.java
@@ -0,0 +1,13 @@
+package org.alonso.rrhhapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrhhAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrhhAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/alonso/rrhhapp/RrhhAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alonso.rrhhapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrhhAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
